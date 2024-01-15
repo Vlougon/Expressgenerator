e312224c4a3a5100aa38a64eaeda5fd1f8448754
Victor <vlougongs@gmail.com>
2024-01-15 21:54:28 +0000

    Removed Nodemon from start!

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
+/* Instantiations */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const favicon = require('express-favicon');
+/* Instantiations */
 
+/* Routes */
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+/* Routes */
 
 var app = express();
 
-// view engine setup
+/* Configurations: view engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+/* Configurations */
 
+/* Middleware */
+app.use(favicon(__dirname + '/public/favicon.png'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+/* Middleware */
 
+/* Error handlers */
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +46,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+/* Error handlers */
 
+/* Server bootup or server export */
 module.exports = app;
+/* Server bootup or server export */
